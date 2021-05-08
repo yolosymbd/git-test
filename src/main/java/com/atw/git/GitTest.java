c289a6ddf585b8834fafd36e1bebc183a9a0acaf
yolo <yolo@atwei.com>
2021-05-08 16:45:44 +0800

    six 1master1

--- src/main/java/com/atw/git/GitTest.java
+++ src/main/java/com/atw/git/GitTest.java
@@ -4,8 +4,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");//“”.sout
         System.out.println("hello,git");//“”.sout
-        System.out.println("hello,git3");//“”.sout
-        System.out.println("hello,git4");//“”.sout
-        System.out.println("hello,hottest");//“”.sout
     }
 }
